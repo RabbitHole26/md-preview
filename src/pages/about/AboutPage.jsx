2104c97a048e161282773a20ddfa6f98420a07c7
Jakub <derivatives_four@protonmail.com>
2024-08-02 22:21:50 +0200

    refactor About page: complete overhaul

--- src/pages/about/AboutPage.jsx
+++ src/pages/about/AboutPage.jsx
@@ -1,28 +1,55 @@
-import { Link } from "react-router-dom"
+import { light } from "../../store/theme-context/theme-aliases-map"
+import useAuthContext from '../../store/auth-context/useAuthContext'
+import useThemeContext from "../../store/theme-context/useThemeContext"
 import ButtonPrimary from "../../components/buttons/ButtonPrimary"
-import scrollToTop from "../../utils/scroll-to-top"
+import LinkCustom from "../../components/link-custom/LinkCustom"
 
 const AboutPage = () => {
+  const {session} = useAuthContext()
+  const {theme} = useThemeContext()
+
   return (
-    <div className="flex flex-col gap-8 sm:gap-[60px] px-3 pt-5 pb-20 items-center sm:mx-[10%]">
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium eaque in omnis aliquid consequatur. Reprehenderit excepturi expedita laudantium necessitatibus iste illo possimus omnis placeat atque, exercitationem ad praesentium, pariatur asperiores!
-        Asperiores id quae vel! Rem porro dolores maxime exercitationem id! Tenetur reprehenderit aliquam odit fuga vitae omnis distinctio impedit sint officiis illo iusto ut, eum rem libero? Veritatis, odio veniam.
-        Fugiat eum laudantium, aliquam perspiciatis iure reprehenderit! Eius numquam rerum itaque inventore tempore necessitatibus libero velit ipsum accusamus eveniet magnam saepe quo mollitia suscipit animi sit, aliquid alias nostrum similique!
-        Eveniet earum, reprehenderit odio in iste ea officiis? Sapiente nostrum perspiciatis dicta impedit nesciunt sit debitis aspernatur quas, ipsa maiores voluptate dolorem? Nam velit numquam ullam fuga porro praesentium non!
-        Possimus praesentium, hic iure sed saepe, architecto reprehenderit, numquam eos incidunt natus voluptatum quidem delectus vel odit blanditiis labore vitae sequi? Quaerat, quos! Sed earum qui quos assumenda eaque beatae.
-        Natus corporis aliquid ut dolorem velit excepturi vero praesentium nulla animi consectetur, quibusdam quo repellendus eligendi unde. Fuga laboriosam distinctio corrupti, veritatis asperiores dicta cumque et, repudiandae omnis ipsum temporibus.
-        Nobis perspiciatis voluptas, adipisci tenetur omnis velit sint modi iusto quasi perferendis aperiam porro pariatur eos voluptatem voluptate provident possimus tempora ex consequuntur saepe quas repudiandae quidem. Temporibus, doloribus doloremque?
-        Maxime repellat voluptatum, enim expedita, deserunt vel nulla ut quibusdam dolorum cupiditate esse consectetur laborum rem non cumque excepturi nisi sunt, modi amet quam ea nihil earum. Voluptatibus, suscipit omnis.
-        Ea assumenda corrupti adipisci, facere, molestiae veritatis enim nam voluptatem eveniet odio fugit et unde pariatur itaque? Repellendus enim eligendi sint iusto libero excepturi, illum eaque accusantium laborum in voluptas!
-        Cum, vel nam aut quae, ut perferendis tempora molestias excepturi cumque atque sint eius aliquam recusandae, adipisci veritatis expedita. Nemo aliquid dolorem culpa perferendis alias amet impedit deleniti eveniet magni?
-      </p>
-      {/* for some reason when it redirects to main page, the page is scrolled down, so had to force it to redirect and then scroll up. */}
-      <Link onClick={scrollToTop} to='/'>
+    <div className="flex flex-col gap-8 sm:gap-[60px] mx-3 pt-5 pb-20 items-center sm:mx-[10%] lg:mx-[20%] 2xl:mx-[30%]">
+      <div className="sm:mt-10 flex flex-col gap-6 md:gap-10">
+        <h1 className="text-xl lg:text-2xl font-bold text-center">Welcome to MD-Preview!</h1>
+        <p>
+          This is your go-to markdown editor with live preview! This app makes it easy to create, edit, and manage your markdown snippets all in one place. Whether you&apos;re jotting down quick notes or drafting longer content, MD-Preview has you covered.
+        </p>
+        <ul className="mx-6 330px-custom:mx-[10%]">
+          <li>Store, edit, and preview markdown snippets.</li>
+          <li>Rename, delete, and organize your snippets effortlessly.</li>
+          <li>Enjoy dynamic local storage that saves your work automatically, even if you refresh or open new tabs.</li>
+          <li>Switch between light and dark modes to suit your preference.</li>
+          <li>Sync your snippets across all your devices with just a click.</li>
+          <li>Manage your account settings, including username and password changes.</li>
+        </ul>
+        {!session
+          ? (
+            <p>
+              <LinkCustom className="underline has-hover:hover:text-primary" to='/signup'>
+                Create an account
+              </LinkCustom>
+              {' '}
+              using your email address to access your snippets from any device, anytime you&apos;re
+              {' '}
+              <LinkCustom className="underline has-hover:hover:text-primary" to='/login'>
+                logged in
+              </LinkCustom>
+              . With MD-Preview, you can seamlessly continue your work wherever you are.
+            </p>
+          )
+          : (
+            <p>
+              You can access your snippets from any device, anytime you&apos;re logged in. With MD-Preview, you can seamlessly continue your work wherever you are.
+            </p>
+          )
+        }
+      </div>
+      <LinkCustom className={`sticky bottom-0 p-6 rounded-btn ${theme === light ? 'bg-radial-gradient-theme-light' : 'bg-radial-gradient-theme-dark'}`} to='/'>
         <ButtonPrimary>
           Back to homepage
         </ButtonPrimary>
-      </Link>
+      </LinkCustom>
     </div>
   )
 }
