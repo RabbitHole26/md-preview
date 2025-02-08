806c3d2162ed071268bd0d0c0ddb1483657f97e4
RabbitHole26 <derivatives_four@protonmail.com>
2025-02-08 17:56:41 +0100

    feat: add index page for Settings page in Dashboard
    - The user is now redirected to the 'Change display name' page when visiting the 'Settings page' in the 'Dashboard'. This change prevents rendering of the 'DisplaySettingsMain' component which is currently unfinished.

--- src/components/dashboard/DashboardSettings.jsx
+++ src/components/dashboard/DashboardSettings.jsx
@@ -1,16 +1,17 @@
-import { Route, Routes } from "react-router-dom"
+import { Route, Routes, Navigate } from "react-router-dom"
 import ChangePasswordForm from "../dashboard-forms/ChangePasswordForm"
 import DashboardSettingsLayout from "../../layouts/DashboardSettingsLayout"
 import ChangeDisplayNameForm from "../dashboard-forms/ChangeDisplayNameForm"
 import ErrorPage from "../../pages/error/ErrorPage"
-import DashboardSettingsMain from "./DashboardSettingsMain"
+// import DashboardSettingsMain from "./DashboardSettingsMain"
 
 const DashboardSettings = () => {
   return (
     <div>
       <DashboardSettingsLayout>
         <Routes>
-          <Route path="/" element={<DashboardSettingsMain />} />
+          {/* <Route path="/" element={<DashboardSettingsMain />} /> */}
+          <Route index element={<Navigate to="/dashboard/settings/display-name" replace />}/>
           <Route path="/display-name" element={<ChangeDisplayNameForm />}/>
           <Route path="/password" element={<ChangePasswordForm />}/>
           <Route path="*" element={<ErrorPage />}/>
